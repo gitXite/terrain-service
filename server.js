7e2dc757629bb03493c467eb3f214fc94e46717e
gitXite <daniel.halaas@gmail.com>
2025-09-11 15:14:37 +0200

    Comment out middleware for the time being

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
 
-const checkApiKey = require('./src/middleware/apiKey');
-const rateLimiter = require('./src/middleware/rateLimiter');
+// const checkApiKey = require('./src/middleware/apiKey');
+// const rateLimiter = require('./src/middleware/rateLimiter');
 
 
 const app = express();
 app.use(express.json());
 
-app.use(checkApiKey);
-app.use('/generate', rateLimiter);
+// app.use(checkApiKey);
+// app.use('/generate', rateLimiter);
 
 
 const PORT = process.env.PORT || 8080;
